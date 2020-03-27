87b14427a2af6e4067a60fd7943f3d5d767379ec
willw625731 <willwombell@gmail.com>
2020-03-27 20:18:50 +0000

    First commit.

---
+++ pytorch_models.py
@@ -0,0 +1,175 @@
+import torch
+from torch import nn
+
+
+class Perceptron(nn.Module):
+    def __init__(self):
+        super().__init()
+        
+
+class MLP_Regression(nn.Module):
+    def __init__(self):
+        super().__init()
+        
+        
+class MLP_Classification(nn.Module):
+    def __init__(self):
+        super().__init()
+        
+
+class NN(nn.Module):
+    def __init__(self):
+        super().__init()
+        
+        
+class CNN(nn.Module):
+    def __init__(self):
+        super().__init()
+        
+        
+class Multi_Channel_CNN(nn.Module):
+    def __init__(self):
+        super().__init()
+        
+        
+class nD_CNN(nn.Module):
+    def __init__(self):
+        super().__init()
+        
+        
+class one_by_one_CNN(nn.Module):
+    def __init__(self):
+        super().__init()
+        
+        
+class Transpose_CNN(nn.Module):
+    def __init__(self):
+        super().__init()
+        
+        
+class Separable_CNN(nn.Module):
+    def __init__(self):
+        super().__init()
+        
+        
+class Flattened_CNN(nn.Module):
+    def __init__(self):
+        super().__init()
+        
+        
+class Grouped_CNN(nn.Module):
+    def __init__(self):
+        super().__init()
+        
+        
+class ShuffledGrouped_CNN(nn.Module):
+    def __init__(self):
+        super().__init()
+        
+        
+class PointwiseGrouped_CNN(nn.Module):
+    def __init__(self):
+        super().__init()
+        
+        
+class Residual_CNN(nn.Module):
+    def __init__(self):
+        super().__init()
+        
+        
+class Dense_CNN(nn.Module):
+    def __init__(self):
+        super().__init()
+        
+                
+class Vanilla_AE(nn.Module):
+    def __init__(self):
+        super().__init()
+        
+        
+class Multilayer_AE(nn.Module):
+    def __init__(self):
+        super().__init()
+        
+        
+class Sparse_AE(nn.Module):
+    def __init__(self):
+        super().__init()
+        
+        
+class Denoising_AE(nn.Module):
+    def __init__(self):
+        super().__init()
+        
+        
+class Convolutional_AE(nn.Module):
+    def __init__(self):
+        super().__init()
+        
+        
+class Variational_AE(nn.Module):
+    def __init__(self):
+        super().__init()
+        
+        
+class Undercomplete_AE(nn.Module):
+    def __init__(self):
+        super().__init()
+        
+        
+class Residual_NN(nn.Module):
+    def __init__(self):
+        super().__init()
+        
+        
+class Dense_NN(nn.Module):
+    def __init__(self):
+        super().__init()
+        
+        
+class GAN(nn.Module):
+    def __init__(self):
+        super().__init()
+        
+        
+class RNN_Model(nn.Module):
+    def __init__(self):
+        super().__init()
+        
+        
+class LSTM_Model(nn.Module):
+    def __init__(self):
+        super().__init()
+        
+        
+class GRU_Model(nn.Module):
+    def __init__(self):
+        super().__init()
+        
+        
+class ReinforcementLearning_Model(nn.Module):
+    def __init__(self):
+        super().__init()
+        
+        
+class Transformer(nn.Module):
+    def __init__(self):
+        super().__init()
+        
+        
+class Transformer_Encoder(nn.Module):
+    def __init__(self):
+        super().__init()
+        
+        
+class Transformer_Decoder(nn.Module):
+    def __init__(self):
+        super().__init()
+        
+        
+class Attention(nn.Module):
+    def __init__(self):
+        super().__init()
+        
+
+        
---
+++ tensorflow_models.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+
+# Models
+
+
+# Tabulated data
+# -> NN regression
+# -> NN classification
+# -> Graph transformer
+
+# Vision
+# -> CNN
+# -> Residual CNN
+# -> Dense CNN
+# -> Grouped CNN
+# -> Full CNN
+
+# Language
+# -> Attention
+# -> Transformer Encoder
+# -> Transformer Decoder
+
+# Unsupervised
+# -> Autoencoder
+# -> Variational-AE
+# -> Sparse-AE
+# -> Contractive-AE
+# -> Denoising-AE
+
+# Utility
+# -> Autoregressive optimizer
